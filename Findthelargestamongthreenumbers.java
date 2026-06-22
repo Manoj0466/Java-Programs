5676c3380fe957fe5223e29b4cef647d2c5da62f
Manoj Kumar <polepallimanojkumar2005@gmail.com>
2026-06-22 17:05:41 +0530

    added java programs

--- Findthelargestamongthreenumbers.java
+++ Findthelargestamongthreenumbers.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Main{
+public class Findthelargestamongthreenumbers {
     public static void main(String[] args){
         Scanner sc =new Scanner(System.in);
         System.out.print("Enter the 1st number: ");
